87a3547e33a704071d52d24c225c7e801776c060
annoinspace <aneesahalmask@gmail.com>
2022-12-08 20:35:58 +0000

    card detail complete

--- src/components/SingleArticleCard.tsx
+++ src/components/SingleArticleCard.tsx
@@ -1,6 +1,8 @@
 import React from "react"
-import { Card } from "react-bootstrap"
+import { Card, Button } from "react-bootstrap"
 import { NewsArticle } from "../types"
+import Moment from "react-moment"
+import { useNavigate } from "react-router-dom"
 
 interface SingleNewsArticleProps {
   singleArticleDetail: NewsArticle
@@ -9,22 +11,34 @@ interface SingleNewsArticleProps {
 export default function SingleArticleCard({
   singleArticleDetail
 }: SingleNewsArticleProps) {
+  const navigate = useNavigate()
   return (
     <div className="d-flex justify-content-center">
       <Card style={{ width: "55rem" }}>
-        <>
-          {/* <div>First Published {singleArticleDetail.publishedAt}</div> */}
-          {/* <div>First Published: {singleArticleDetail.publishedAt}</div> */}
-        </>
+        <Card.Text className="d-flex justify-content-around mt-3">
+          <div>
+            <span>Published on: </span>
+            <Moment format="DD/MM/YYYY">
+              {singleArticleDetail.publishedAt}
+            </Moment>
+          </div>
+
+          <div>
+            <span>Updated on: </span>
+            <Moment format="DD/MM/YYYY">{singleArticleDetail.updatedAt}</Moment>
+          </div>
+        </Card.Text>
         <Card.Img variant="top" src={singleArticleDetail.imageUrl} />
         <Card.Body>
           <Card.Text>{singleArticleDetail.summary}</Card.Text>
         </Card.Body>
+        <Card.Body></Card.Body>
 
         <Card.Body className="d-flex justify-content-around">
           <>
-            <div>Card Link</div>
-            <div>Another Link</div>
+            <Button variant="info" onClick={() => navigate("/")}>
+              Back To Home
+            </Button>
           </>
         </Card.Body>
       </Card>
--- src/types/index.ts
+++ src/types/index.ts
@@ -11,17 +11,3 @@ export interface NewsArticle {
   launches?: any[]
   events?: any[]
 }
-
-// export interface SingleNewsArticle {
-//   id?: number
-//   title?: string
-//   url?: string
-//   imageUrl?: string
-//   newsSite?: string
-//   summary?: string
-//   publishedAt?: Date
-//   updatedAt?: Date
-//   featured?: boolean
-//   launches?: any[]
-//   events: any[]
-// }
